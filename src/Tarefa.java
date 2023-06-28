27ac771657feb7230d85f890177d972f71643db4
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-06-28 10:38:47 -0400

    Commit final

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -4,20 +4,20 @@ public class Tarefa {
     /**
      *  Classe Scanner para ler a entrada do usuário a partir do console. Primeiro, solicitamos ao usuário que digite um valor numérico. Em seguida, usamos o método nextInt() para ler um valor inteiro da entrada.
      *
-     * Em seguida, fazemos a conversão do valor inteiro para double usando (double) valorInteiro. Isso é conhecido como "casting" e é usado para converter um tipo de dado em outro tipo compatível.
+     * Em seguida, fazemos a conversão do valor inteiro para Double usando a classe Double. Isso é conhecido como "boxing" e é usado para converter um tipo primitivo em um objeto do tipo wrapper.
      *
-     * Por fim, imprimimos o valor convertido para double no console usando System.out.println().
+     * Por fim, imprimimos o valor convertido para Double no console usando System.out.println().
      * @param args
      */
     public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Digite um valor numérico: ");
-            int valorInteiro = scanner.nextInt();
+        System.out.println("Digite um valor numérico: ");
+        int valorInteiro = scanner.nextInt();
 
-            double valorDouble = (double) valorInteiro;
+        Double valorDouble = Double.valueOf(valorInteiro);
 
-            System.out.println("Valor convertido para double: " + valorDouble);
+        System.out.println("Valor convertido para Double: " + valorDouble);
     }
 }
